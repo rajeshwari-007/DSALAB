422b01ad2891006ee8c8857612672aef04b409f1
rajeshwari-007 <rajeshwarimd25@gmail.com>
2025-10-10 15:45:04 +0530

    Add files via upload

---
+++ TOH.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void TOH(int n,char s,char d,char t)
+{
+    if(n==1)
+        printf("\n move %d disk from %c to %c",n,s,d);
+    else
+    {
+        TOH(n-1,s,t,d);
+        printf("\n move %d disk from %C to %c",n,s,d);
+        TOH(n-1,t,d,s);
+    }
+}
+    int main()
+    {
+        int n;
+        printf("\n read number of disk:");
+        scanf("%d",&n);
+        TOH(n,'S','D','T');
+        return 0;
+    }
+
